6871621a08ece7c9233a0c651bc1d5e10bccff81
Gangas81 <142255224+Gangas81@users.noreply.github.com>
2024-11-19 21:36:52 +0200

    updated register file

--- register.py
+++ register.py
@@ -21,36 +21,35 @@ def data_insert():
             return jsonify({"message": "Username should not contain spaces"}), 400
         # Check for spaces in the password
         elif ' ' in password:
-            return jsonify({"message": "password should not contain spaces"}), 400
-
+            return jsonify({"message": "Password should not contain spaces"}), 400
 
         # Check if users.json exists, create it if not
         if not os.path.isfile("users.json"):
             with open("users.json", "w") as file:
-                json.dump({"users": []}, file, indent=4)
-        
+                json.dump({"credentials": []}, file, indent=4)
+
         # Load JSON data from a file
         with open("users.json", "r") as file:
-            data = json.load(file)
+            users = json.load(file)
 
-        # Extract the list of users
-        users = data.get("users", [])
+        # Extract the credentials list
+        credentials = users.get("credentials", [])
 
         # Check if the username already exists
-        if any(user["username"] == username for user in users):
-             return jsonify({"message": "Username is taken"})
-
+        if any(user["username"] == username for user in credentials):
+            return jsonify({"message": "Username is taken"})
         else:
             print("Username is available. You can add it.")
             # Add the new user
-            users.append({"username": username, "password": password})
+            credentials.append({"username": username, "password": password})
 
             # Save the updated data back to the JSON file
+            users["credentials"] = credentials
             with open("users.json", "w") as file:
-                json.dump({"users": users}, file, indent=4)
+                json.dump(users, file, indent=4)
 
         return jsonify({"message": "User registered successfully"})
-    
+
     except Exception as e:
         print(f"An error occurred: {e}")
         return jsonify({"message": f"An error occurred: {e}"}), 500
