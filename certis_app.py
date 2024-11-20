4ea6a0726951d49170279801f200055c88423bb9
shomronh <shomronh@gmail.com>
2024-11-20 21:33:45 +0200

    certi app singleton

---
+++ certis_app.py
@@ -0,0 +1,53 @@
+import threading
+
+from flask import Flask
+
+class CertisApp:
+
+    # static variables
+    _instance: 'CertisApp'  = None
+
+    # other variables
+
+    @staticmethod
+    def get_instance():
+        if not CertisApp._instance:
+            CertisApp._instance = CertisApp()
+            CertisApp._instance.__start()
+        return CertisApp._instance
+
+    # private method
+    def __start(self):
+        self._app = Flask(__name__)
+
+        # will block the access to other methods
+        # self.app.run(host='0.0.0.0', port=8080)
+
+        # solution:
+        # run flask in the background
+        self._thread = threading.Thread(target=self._app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
+        self._thread.daemon = True  # Daemon thread will exit when the main program exits
+        self._thread.start()
+        # thread.join()
+
+        # yes we can create routes after running the flask app
+
+        # http://127.0.0.1:8080/hello
+        @self._app.route('/hello', methods=['GET'])
+        def say_hello():
+            return 'hello'
+
+        @self._app.route('/', methods=['GET'])
+        def main():
+            return 'hello1'
+
+    def get_app(self):
+        return self._app
+
+    def join(self):
+        self._thread.join()
+
+
+if __name__ == '__main__':
+    app = CertisApp().get_instance()
+    app.join()
