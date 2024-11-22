65e5bfb6ddaca98003511f93f05bf9a29569147d
ספיר אלמליח <matanyamelmaliah@gmail.com>
2024-11-22 22:38:57 +0200

    edit register.py with new class

--- register_api.py
+++ register_api.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask, render_template, request, jsonify
 import os
-
+from UserRepository import UserRepository
 
 
 class RegisterApi:
@@ -28,31 +28,10 @@ class RegisterApi:
                     return jsonify({"message": "Password should not contain spaces"}), 400
 
                 # Check if users.json exists, create it if not
-                if not os.path.isfile("users.json"):
-                    with open("users.json", "w") as file:
-                        json.dump({"credentials": []}, file, indent=4)
+                UserRepository.check_if_file_exist()
 
                 # Load JSON data from a file
-                with open("users.json", "r") as file:
-                    users = json.load(file)
-
-                # Extract the credentials list
-                credentials = users.get("credentials", [])
-
-                # Check if the username already exists
-                if any(user["username"] == username for user in credentials):
-                    return jsonify({"message": "Username is taken"})
-                else:
-                    print("Username is available. You can add it.")
-                    # Add the new user
-                    credentials.append({"username": username, "password": password})
-
-                    # Save the updated data back to the JSON file
-                    users["credentials"] = credentials
-                    with open("users.json", "w") as file:
-                        json.dump(users, file, indent=4)
-
-                return jsonify({"message": "User registered successfully"})
+                UserRepository.register(username,password)
 
             except Exception as e:
                 print(f"An error occurred: {e}")
