0f2daaa1e8a1863e51bf3174e631fefb0cf7bb2a
ספיר אלמליח <matanyamelmaliah@gmail.com>
2024-11-23 17:07:43 +0200

    add UserRepository class login logic to api

--- login_api.py
+++ login_api.py
@@ -1,5 +1,7 @@
 import json
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+from UserRepository import UserRepository
+
 
 # app = Flask(__name__)
 # app.secret_key = "IxfTJGSMIs{um"
@@ -11,34 +13,24 @@ class LoginApi:
         # rest apis
         @app.route('/login', methods=['POST'])
         def login():
-            try:
-                # Open and read the JSON file
-                with open('users.json', 'r') as file:
-                    user_data = json.load(file)
 
-                data = request.get_json()
+            data = request.get_json()
 
-                if not data:
-                    return jsonify({"message": "Please enter credentials"}), 400
+            if not data:
+                return jsonify({"message": "Please enter credentials"}), 400
 
-                username = data.get('username')
-                password = data.get('password')
+            username = data.get('username')
+            password = data.get('password')
 
-                # Check credentials
-                for user in user_data['credentials']:
-                    if username == user['username'] and password == user['password']:
-                        session["username"] = username  # Store username in session
-                        return jsonify({"message": "Login successful"}), 200
-
-                # If credentials do not match
-                return jsonify({"message": "Invalid credentials, please try again"}), 401
-
-            except FileNotFoundError:
-                print("File not found error.")
-                return jsonify({"message": "Please register before trying to log in"}), 400
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                return jsonify({"message": f"An error occurred: {e}"}), 500
+            user = UserRepository()
+            answer = user.login(username, password)
+            #Checks the answer and returns the status code with relevant response
+            if answer == "Login successful":
+                return jsonify({"message": answer}), 200
+            elif answer == "Please register before trying to log in":
+                return jsonify({"message": answer}), 404  # Missing file, send 404 status
+            else:
+                return jsonify({"message": answer}), 401  # Invalid credentials
 
         # Visual Routes
         @app.route('/dashboardPage')
