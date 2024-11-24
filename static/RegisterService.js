2858eb238242ca15436b9f3188b1104897e5481a
shomronh <shomronh@gmail.com>
2024-11-24 19:45:03 +0200

    add usage of RegisterService

---
+++ static/RegisterService.js
@@ -0,0 +1,32 @@
+
+
+async function dataInsert(username, password) {
+    try {
+        const response = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: username, password: password })
+        });
+
+        console.log('Fetch response:', response);
+
+        const data = await response.json();
+
+        if (data.message === "User registered successfully") {
+            alert("User registered successfully");
+            window.location.href = "/loginPage";
+        }else if (data.message === "Username is taken") {
+            alert("Username is already taken. Please choose a different one.");
+        }else if (data.message === "Username should not contain spaces"||data.message ==="password should not contain spaces") {
+            alert("canot contain spaces");
+        }
+
+        console.log('Response data:', data);
+       
+
+    } catch (error) {
+        console.error('Error during fetch:', error);
+    }
+}
--- static/registerPage.js
+++ static/registerPage.js
@@ -8,49 +8,8 @@ form.addEventListener('submit', function(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Check for spaces in the username
-    // if (username.includes(' ')) {
-    //     alert("Username should not contain spaces");
-    //     return; // Stop the form submission
-    // }
-    // if (password.includes(' ')) {
-    //     alert("password should not contain spaces");
-    //     return; // Stop the form submission
-    // }
-
     event.target.reset();
     
     console.log(`Username: ${username}, Password: ${password}`);
     dataInsert(username, password);
 });
-
-async function dataInsert(username, password) {
-    try {
-        const response = await fetch('/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username: username, password: password })
-        });
-
-        console.log('Fetch response:', response);
-
-        const data = await response.json();
-
-        if (data.message === "User registered successfully") {
-            alert("User registered successfully");
-            window.location.href = "/loginPage";
-        }else if (data.message === "Username is taken") {
-            alert("Username is already taken. Please choose a different one.");
-        }else if (data.message === "Username should not contain spaces"||data.message ==="password should not contain spaces") {
-            alert("canot contain spaces");
-        }
-
-        console.log('Response data:', data);
-       
-
-    } catch (error) {
-        console.error('Error during fetch:', error);
-    }
-}
