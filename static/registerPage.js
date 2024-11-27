a0fd8328372ddda7090e092b2e5cf425ffa94527
shomronh <shomronh@gmail.com>
2024-11-27 17:34:39 +0200

    more fixes

--- static/registerPage.js
+++ static/registerPage.js
@@ -1,5 +1,5 @@
 
-const form = document.getElementById('login-form');
+const form = document.getElementById('register-form');
 const status = document.getElementById('status');
 
 form.addEventListener('submit', function(event) {
