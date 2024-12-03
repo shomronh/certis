ed8a8235634c8d9431db3de13db11416583e37dc
shomronh <shomronh@gmail.com>
2024-12-03 17:55:20 +0200

    automation file

---
+++ __automation/installer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e 
+
+# enter sudo user
+
+sudo -i
+
+apt -y update && apt -y install python3-pip
+
+cd /
+
+git clone https://github.com/shomronh/certis.git
+echo "clone https://github.com/shomronh/certis.git completed"
+
+
+BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
+sed -i "s/^BACKEND_URL=.*/BACKEND_URL=$BACKEND_URL_VALUE/" .env.prod
+echo "update BACKEND_URL env value for .env.prod file"
+
+git checkout dev
+echo "switched to dev branch completed"
+
+cp /certis/__automation/certis_app.sh /usr/local/bin/certis_app.sh 
+cp /certis/__automation/certis.service /lib/systemd/system/certis.service
+echo "copied certis_app and certis.service to targets completed"
+
+pip install -r requirements.txt --break-system-packages
+echo "install requirements.txt completed"
+
+systemctl start ufw
+sudo ufw allow 8080/tcp
+
+systemctl daemon-reload 
+systemctl stop certis
+systemctl start certis
+systemctl enable certis
+systemctl status certis
+
+
+
+
+
+
