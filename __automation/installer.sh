0ceb167f8b82e0752735435d0946b173a353e439
shomronh <shomronh@gmail.com>
2024-12-03 18:24:44 +0200

    automation changes

--- __automation/installer.sh
+++ __automation/installer.sh
@@ -23,7 +23,7 @@ echo "switched to dev branch completed"
 chmod +x ./__automation/certis_app.sh
 
 BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
-sed -i "s/^BACKEND_URL=.*/BACKEND_URL=$BACKEND_URL_VALUE/" .env.prod
+sed -i "s/^BACKEND_URL=.*/BACKEND_URL=https://$BACKEND_URL_VALUE:8080/" .env.prod
 echo "update BACKEND_URL env value for .env.prod file"
 
 cp -f ./__automation/certis_app.sh /usr/local/bin/certis_app.sh 
