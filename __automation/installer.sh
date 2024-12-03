5aef90893698f0f23bc68936afd8124ba2667d02
shomronh <shomronh@gmail.com>
2024-12-03 18:22:25 +0200

    automation changes

--- __automation/installer.sh
+++ __automation/installer.sh
@@ -10,6 +10,8 @@ apt -y update && apt -y install python3-pip
 
 cd /
 
+rm -rf /certis
+
 git clone https://github.com/shomronh/certis.git
 echo "clone https://github.com/shomronh/certis.git completed"
 
@@ -18,12 +20,14 @@ cd /certis
 git checkout dev
 echo "switched to dev branch completed"
 
+chmod +x ./__automation/certis_app.sh
+
 BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
 sed -i "s/^BACKEND_URL=.*/BACKEND_URL=$BACKEND_URL_VALUE/" .env.prod
 echo "update BACKEND_URL env value for .env.prod file"
 
-cp /certis/__automation/certis_app.sh /usr/local/bin/certis_app.sh 
-cp /certis/__automation/certis.service /lib/systemd/system/certis.service
+cp -f ./__automation/certis_app.sh /usr/local/bin/certis_app.sh 
+cp -f ./__automation/certis.service /lib/systemd/system/certis.service
 echo "copied certis_app and certis.service to targets completed"
 
 pip install -r requirements.txt --break-system-packages
@@ -31,12 +35,14 @@ echo "install requirements.txt completed"
 
 systemctl start ufw
 sudo ufw allow 8080/tcp
+echo "firewall configured to use port 8080"
 
-systemctl daemon-reload 
 systemctl stop certis
+systemctl daemon-reload 
 systemctl start certis
 systemctl enable certis
 systemctl status certis
+echo "certis.service has started and enabled"
 
 
 
