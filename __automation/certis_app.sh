0a542c7913330d5594cb8a1a866eaf6d31812b0f
shomronh <shomronh@gmail.com>
2024-12-09 01:32:30 +0200

    update automation to take into account the domain of the ec2

--- __automation/certis_app.sh
+++ __automation/certis_app.sh
@@ -4,9 +4,9 @@
 #set -x
 #env
 
-# BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
-# sed -i "s|^BACKEND_URL=.*|BACKEND_URL=http://$BACKEND_URL_VALUE:8080|" .env.prod
-# echo "update BACKEND_URL env value for .env.prod file"
+BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
+sed -i "s|^BACKEND_URL=.*|BACKEND_URL=http://ec2-$BACKEND_URL_VALUE.us-west-2.compute.amazonaws.com:8080|" .env.prod
+echo "update BACKEND_URL env value for .env.prod file"
 
 export CERTIS_BACKEND_ENV=prod
 
