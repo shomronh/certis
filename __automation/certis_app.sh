f2d5ee1eed487d9417ebc806e25e43c952bf1044
shomronh <shomronh@gmail.com>
2024-12-09 01:49:28 +0200

    changes

--- __automation/certis_app.sh
+++ __automation/certis_app.sh
@@ -5,7 +5,11 @@
 #env
 
 BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
-BACKEND_URL_VALUE=$BACKEND_URL_VALUE | sed 's/\./-/g'
+
+# replace dots of IP address with dashs
+BACKEND_URL_VALUE=$(echo "$BACKEND_URL_VALUE" | sed 's/\./-/g')
+
+
 sed -i "s|^BACKEND_URL=.*|BACKEND_URL=http://ec2-$BACKEND_URL_VALUE.us-west-2.compute.amazonaws.com:8080|" .env.prod
 echo "update BACKEND_URL env value for .env.prod file"
 
