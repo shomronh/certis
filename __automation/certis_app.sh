f608a39d23340f4bb06db9cf3ce3187a7bf0b636
shomronh <shomronh@gmail.com>
2024-12-08 18:52:40 +0200

    disable dynamic ip fetching insert it manually into prod env

--- __automation/certis_app.sh
+++ __automation/certis_app.sh
@@ -4,9 +4,9 @@
 #set -x
 #env
 
-BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
-sed -i "s|^BACKEND_URL=.*|BACKEND_URL=http://$BACKEND_URL_VALUE:8080|" .env.prod
-echo "update BACKEND_URL env value for .env.prod file"
+# BACKEND_URL_VALUE=$(curl -s http://checkip.amazonaws.com)
+# sed -i "s|^BACKEND_URL=.*|BACKEND_URL=http://$BACKEND_URL_VALUE:8080|" .env.prod
+# echo "update BACKEND_URL env value for .env.prod file"
 
 export CERTIS_BACKEND_ENV=prod
 
